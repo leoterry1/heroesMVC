2f265f91e6c0de866a09994926aa4b88cc65e0ad
Leo Terry <lucasenriquez1997@gmail.com>
2020-11-27 18:02:56 -0300

    Les agregué a los textos <br> para que se vean mejor

--- controllers/heroesController.js
+++ controllers/heroesController.js
@@ -2,37 +2,37 @@ const fs = require("fs")
 const heroes = JSON.parse(fs.readFileSync("./data/heroes.json", "utf-8"))
 
 module.exports = {
-    data : function (req, res) {
+    data: function (req, res) {
         res.send(heroes)
     },
-    bio : function (req,res){
-    let id = req.params.idHeroe;
-    let ok = req.params.ok;
-    let heroe = heroes.filter((heroe) => {
-		return heroe.id == id
-    });
-	let heroeSeleccionado = heroe[0];
-    if (heroeSeleccionado == undefined){
-        return res.send("No encontramos un héroe para mostrarte su biografía")
-    }
-    if (ok == "ok"){
-        return res.send(`${heroeSeleccionado.nombre}: ${heroeSeleccionado.resenia}`)
-    } else {
-        return res.send(`${heroeSeleccionado.nombre}: Lamento que no desees saber más de mí.`)
-    }
+    bio: function (req, res) {
+        let id = req.params.idHeroe;
+        let ok = req.params.ok;
+        let heroe = heroes.filter((heroe) => {
+            return heroe.id == id
+        });
+        let heroeSeleccionado = heroe[0];
+        if (heroeSeleccionado == undefined) {
+            return res.send("<br>No encontramos un héroe para mostrarte su biografía")
+        }
+        if (ok == "ok") {
+            return res.send(`<br>${heroeSeleccionado.nombre}: ${heroeSeleccionado.resenia}`)
+        } else {
+            return res.send(`<br>${heroeSeleccionado.nombre}: Lamento que no desees saber más de mí.`)
+        }
     },
-    heroe : function(req,res){
+    heroe: function (req, res) {
         let id = req.params.idHeroe
 
-	let heroe = heroes.filter((heroe) => {
-		return heroe.id == id
-	});
-	let heroeSeleccionado = heroe[0];
+        let heroe = heroes.filter((heroe) => {
+            return heroe.id == id
+        });
+        let heroeSeleccionado = heroe[0];
 
-	if(heroeSeleccionado == undefined) {
-		res.send('Este héroe no fue encontrado :(  pruebe con otro numero de id.');
-	} else {
-		res.send(`Hola, mi nombre es ${heroeSeleccionado.nombre} y soy ${heroeSeleccionado.profesion}`)
+        if (heroeSeleccionado == undefined) {
+            res.send('<br>Este héroe no fue encontrado :(  pruebe con otro numero de id.');
+        } else {
+            res.send(`<br>Hola, mi nombre es ${heroeSeleccionado.nombre} y soy ${heroeSeleccionado.profesion}`)
+        }
     }
-}   
 }
